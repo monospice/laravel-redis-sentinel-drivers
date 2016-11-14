monospice/laravel-view-composers

Create skeleton project
Signed-off-by: Cy Rossignol <cy@rossignols.me>

tests/SkeletonTest.php
@@ -1,0 +1,21 @@
+<?php
+
+namespace Monospice\Skeleton\Test;
+
+use Monospice\Skeleton\Skeleton;
+
+/**
+ * An example test. Remove this file.
+ *
+ * @author Cy Rossignol <cy@rossignols.me>
+ */
+class SkeletonTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSkeleton()
+    {
+        $skeleton = new Skeleton();
+        $audience = 'world';
+
+        $this->assertSame('Hello, world!', $skeleton->hello());
+    }
+}

monospice/laravel-redis-sentinel-drivers

tests/SkeletonTest.php
@@ -1,0 +1,21 @@
+<?php
+
+namespace Monospice\Skeleton\Tests;
+
+use Monospice\Skeleton\Skeleton;
+
+/**
+ * An example test. Remove this file.
+ *
+ * @author Cy Rossignol <cy@rossignols.me>
+ */
+class SkeletonTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSkeleton()
+    {
+        $skeleton = new Skeleton();
+        $audience = 'world';
+
+        $this->assertSame('Hello, world!', $skeleton->hello());
+    }
+}